3a32c8dfb7744fd01e0f4ad02b0d5b6aab46133f
pop4959 <pop4959@gmail.com>
2020-09-15 15:50:06 -0700

    Minehut patches

--- src/main/java/com/onarandombox/MultiverseCore/MultiverseCore.java
+++ src/main/java/com/onarandombox/MultiverseCore/MultiverseCore.java
@@ -60,7 +60,6 @@ import com.onarandombox.MultiverseCore.commands.PurgeCommand;
 import com.onarandombox.MultiverseCore.commands.RegenCommand;
 import com.onarandombox.MultiverseCore.commands.ReloadCommand;
 import com.onarandombox.MultiverseCore.commands.RemoveCommand;
-import com.onarandombox.MultiverseCore.commands.ScriptCommand;
 import com.onarandombox.MultiverseCore.commands.SetSpawnCommand;
 import com.onarandombox.MultiverseCore.commands.SilentCommand;
 import com.onarandombox.MultiverseCore.commands.SpawnCommand;
@@ -770,7 +769,6 @@ public class MultiverseCore extends JavaPlugin implements MVPlugin, Core {
         this.commandHandler.registerCommand(new SilentCommand(this));
         this.commandHandler.registerCommand(new GeneratorCommand(this));
         this.commandHandler.registerCommand(new CheckCommand(this));
-        this.commandHandler.registerCommand(new ScriptCommand(this));
         this.commandHandler.registerCommand(new GameruleCommand(this));
         this.commandHandler.registerCommand(new GamerulesCommand(this));
     }
@@ -1183,11 +1181,6 @@ public class MultiverseCore extends JavaPlugin implements MVPlugin, Core {
     @Deprecated
     public static MultiverseCoreConfiguration getStaticConfig() {
         return MultiverseCoreConfiguration.getInstance();
-    }
-
-    @Override
-    public Buscript getScriptAPI() {
-        return buscript;
     }
 
     public UnsafeCallWrapper getUnsafeCallWrapper() {
